d64a5b461b936168cb3f3d6ae85426716d4c5405
Mauro Gonzalez <misaacgonzalezandrade@gmail.com>
2024-12-07 17:37:12 -0500

    Update Main.java

--- Beta0.2/Main.java
+++ Beta0.2/Main.java
@@ -2,11 +2,9 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
         AgendaContactos agenda = new AgendaContactos();
         JFrame ventana = new JFrame("Agenda de Contactos");
         ventana.setLayout(null);
